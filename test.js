219e1a308169129d2c2ec94d902bfece26b09a1e
hearhellacopters <31038937+hearhellacopters@users.noreply.github.com>
2023-10-27 02:18:31 -0400

    Delete test.js

--- test.js
+++
@@ -1,3 +0,0 @@
-const {MERSENNETWISTER, MD5, UUID, randomBytes, RANDOMXORSHIFT, SHA1, SHA256, SHA224, SHA384, SHA512, CRC32, CRC3, CRC16} = require('./lib/cjs/index.js')
-const mt = UUID()
-console.log(mt)
