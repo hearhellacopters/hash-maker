080df0eb54dbc49de28da6038ec2ea64f718d171
hellacopters <salpesce@gmail.com>
2023-12-24 14:40:54 -0500

    package fix

--- lib/esm/index.d.ts
+++ lib/esm/index.d.ts
@@ -1,14 +1,10 @@
-export { MERSENNETWISTER } from './MERSENNETWISTER.js';
-export { UUID } from './UUID.js';
-export { MD5 } from './MD5.js';
-export { SHA1 } from './SHA1.js';
-export { SHA224 } from './SHA256.js';
-export { SHA256 } from './SHA256.js';
-export { SHA384 } from './SHA512.js';
-export { SHA512 } from './SHA512.js';
-export { randomBytes } from './randomBytes.js';
-export { CRC32 } from './CRC32.js';
-export { CRC3 } from './CRC32.js';
-export { CRC16 } from './CRC32.js';
-export { RANDOMXORSHIFT } from './RANDOMXORSHIFT.js';
+export { MERSENNETWISTER } from "./MERSENNETWISTER.js";
+export { UUID } from "./UUID.js";
+export { MD5 } from "./MD5.js";
+export { SHA1 } from "./SHA1.js";
+export { randomBytes } from "./randomBytes.js";
+export { RANDOMXORSHIFT } from "./RANDOMXORSHIFT.js";
+export { SHA224, SHA256 } from "./SHA256.js";
+export { SHA384, SHA512 } from "./SHA512.js";
+export { CRC32, CRC3, CRC16 } from "./CRC32.js";
 //# sourceMappingURL=index.d.ts.map
--- src/UUID.ts
+++ src/UUID.ts
@@ -1,4 +1,64 @@
-import {MERSENNETWISTER as mt} from './MERSENNETWISTER.js'
+class RANDOMXORSHIFT {
+    private mt:Uint8Array;
+    constructor(seed?:number|Uint8Array|Buffer) {
+      var s:number;
+      const mt = [0, 0, 0, 0];
+      if (seed == undefined) {
+        seed = new Date().getTime();
+      }
+      if (typeof Buffer !== 'undefined' && seed instanceof Buffer) {
+        if(seed.length < 4){
+            throw new Error("Must be a seed Buffer of 4 bytes")
+        }
+        mt[0] = seed.readUInt32LE() >>> 0;
+      } else
+      if(seed instanceof Uint8Array){
+        if(seed.length < 4){
+            throw new Error("Must be a seed Uint8Array of 4 bytes")
+        }
+        mt[0] = ((seed[3]<< 24) | (seed[2] << 16) | (seed[1] << 8) | seed[0])
+      } else
+      if (typeof seed == "number") {
+        mt[0] = seed >>> 0;
+      }
+  
+      for (var i = 1; i < 5; i++) {
+        s = mt[i - 1] ^ (mt[i - 1] >>> 30);
+        mt[i] =
+          (((((s & 0xffff0000) >>> 16) * 1812433253) << 16) + (s & 0x0000ffff) * 1812433253) + (i - 1);
+        mt[i] >>>= 0;
+      }
+      mt.shift();
+      var result = new Uint8Array(16);
+      mt.forEach((e, i) => {
+        result[(i*4)] = e & 0xFF;
+        result[(i*4) + 1] = (e >> 8) & 0xFF;
+        result[(i*4) + 2] =  (e >> 16) & 0xFF;
+        result[(i*4) + 3] =  (e >> 24) & 0xFF;
+      });
+      this.mt = result;
+    }
+
+    /**
+     * Generate a random unsigned 32 bit integer
+     * @returns number
+     */
+    random_int():number {
+      let v1 = ((this.mt[3]<< 24) | (this.mt[2] << 16) | (this.mt[1] << 8) | this.mt[0])
+      let v4 = ((this.mt[15]<< 24) | (this.mt[14] << 16) | (this.mt[13] << 8) | this.mt[12])
+      let comp_1 = (v4 ^ (v4 >>> 19) ^ v1 ^ (v1 << 11) ^ ((v1 ^ (v1 << 11)) >>> 8)) >>> 0;
+      let new_value = new Uint8Array(4);
+      new_value[0] = comp_1 & 0xFF;
+      new_value[1] = (comp_1 >> 8) & 0xFF;
+      new_value[2] =  (comp_1 >> 16) & 0xFF;
+      new_value[3] =  (comp_1 >> 24) & 0xFF;
+      const shift = this.mt.subarray(4, 16);
+      var newBuffer =  new Uint8Array([...shift, ...new_value]);
+      this.mt = newBuffer;
+      return comp_1;
+    }
+
+}
 
 function getMachineHostname(): string {
     // Check if the code is running in a Node.js environment
@@ -97,7 +157,7 @@ export function UUID(version?:number, options?:Options):string|Buffer|Uint8Array
             buff = seed as Uint8Array
         }
     } else {
-        const random_mt = new mt();
+        const random_mt = new RANDOMXORSHIFT();
         buff = new Uint8Array(16)
         for (let i = 0; i < 16; i++) {
             buff[i] = random_mt.random_int()
