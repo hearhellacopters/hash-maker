6c6cf58e1001e04a52d783822483a16c99a0b379
hellacopters <salpesce@gmail.com>
2024-11-22 04:46:34 -0500

    fixed build issues

---
+++ lib/esm/CRC32.d.ts
@@ -0,0 +1,40 @@
+/// <reference types="node" />
+interface Options32 {
+    asJAMCRC?: boolean;
+    asBuffer?: boolean;
+    asArray?: boolean;
+    asHex?: boolean;
+    asNumber?: boolean;
+}
+/**
+ * Cyclic Redundancy Check 32. Can also return as JAM with options.
+ *
+ * @param {string|Uint8Array|Buffer} message - Message as string, Uint8Array or Buffer
+ * @param {Options32} options - Object with asJAMCRC, asBuffer, asArray, asHex or asNumber as true (default is number)
+ * @returns ``string|Uint8Array|Buffer|number``
+ */
+export declare function CRC32(message: string | Uint8Array | Buffer, options?: Options32): number | string | Uint8Array | Buffer;
+interface Options {
+    asBuffer?: boolean;
+    asArray?: boolean;
+    asHex?: boolean;
+    asNumber?: boolean;
+}
+/**
+ * Cyclic Redundancy Check 3
+ *
+ * @param {string|Uint8Array|Buffer} message - Message as string, Uint8Array or Buffer
+ * @param {Options} options - Object with asBuffer, asArray, asHex or asNumber as true (default is number)
+ * @returns ``string|Uint8Array|Buffer``
+ */
+export declare function CRC3(message: string | Uint8Array | Buffer, options?: Options): number | string | Uint8Array | Buffer;
+/**
+ * Cyclic Redundancy Check 16
+ *
+ * @param {string|Uint8Array|Buffer} message - Message as string, Uint8Array or Buffer
+ * @param {Options} options - Object with asBuffer, asArray, asHex or asNumber as true (default is number)
+ * @returns ``string|Uint8Array|Buffer``
+ */
+export declare function CRC16(message: string | Uint8Array | Buffer, options?: Options): number | string | Uint8Array | Buffer;
+export {};
+//# sourceMappingURL=CRC32.d.ts.map
---
+++ lib/esm/UUID.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="node" />
+interface Options {
+    seed?: Uint8Array | Buffer;
+    mac?: Uint8Array | Buffer;
+    asBuffer?: boolean;
+    asArray?: boolean;
+    asHex?: boolean;
+}
+/**
+ * Generates a UUID as Uint8Array, Buffer or Hex string (default).
+ *
+ * @param {number} version - UUID version 1-5 (default 4)
+ * @param {Options} options - Object with asBuffer, asArray or asHex as true (default is number). If seeding is needed, use ``{seed: seed}``.If a mac ID is needed., use ``{mac: mac}``. Must be UInt8Array or Buffer of 16 bytes.
+ * @returns string
+ */
+export declare function UUID(version?: number, options?: Options): string | Buffer | Uint8Array;
+export {};
+//# sourceMappingURL=UUID.d.ts.map
